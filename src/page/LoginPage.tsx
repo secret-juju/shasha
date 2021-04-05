250e33f295eb084b6a99c8ea65dccef4bb7f3a1c
cutyapple <dbtldhs0915@naver.com>
2021-04-05 22:52:25 +0900

    [FEAT] <src/page/LoginPage.tsx> Apply Login

---
+++ src/page/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as S from './style';
+import Login from '../features/login/Login';
+
+const LoginPage = () => {
+  return (
+    <S.Container>
+      <Login />
+    </S.Container>
+  );
+};
+
+export default LoginPage;
