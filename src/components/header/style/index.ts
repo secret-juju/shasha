e82c7693b122771b2a023761ab1e48f92c070033
cutyapple <dbtldhs0915@naver.com>
2021-04-08 23:16:14 +0900

    [FEAT] <src/component/header/style/index.ts> set css

---
+++ src/components/header/style/index.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  width: 100%;
+  height: 4.375rem;
+  padding: 0 calc(0.75rem + 8vw);
+  background: #4fd98d;
+  box-sizing: border-box;
+`;
+
+export const HeaderLogo = styled.img`
+  width: 7.6875rem;
+  height: 3.4375rem;
+`;
