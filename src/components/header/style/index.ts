56c1b600b1059ee1ff5374850d09175fd3e070ea
cutyapple <dbtldhs0915@naver.com>
2021-04-12 16:05:52 +0900

    [FEAT] <src/components/header/style/index.ts> add header sub nav css

--- src/components/header/style/index.ts
+++ src/components/header/style/index.ts
@@ -12,3 +12,24 @@ export const HeaderLogo = styled.img`
   width: 7.6875rem;
   height: 3.4375rem;
 `;
+
+export const HeaderSubNav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  height: 1.875rem;
+  background: #ffffff;
+  padding: 0 calc(0.75rem + 8vw);
+  box-sizing: border-box;
+`;
+
+export const HeaderAuthButton = styled.button`
+  width: max-content;
+  height: max-content;
+  color: #4b4b4b;
+  font-size: 18px;
+  font-weight: 300;
+  background: none;
+  border: none;
+  padding: 0;
+`;
