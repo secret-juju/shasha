5208dc3e023b4701912196c8865cee524b374cd0
cutyapple <dbtldhs0915@naver.com>
2021-04-12 15:39:33 +0900

    [FEAT] <src/page/main/MainPage.tsx> apply toggle feature

--- src/page/main/MainPage.tsx
+++ src/page/main/MainPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Toggle from '../../features/toggle/Toggle';
+
+import { ToggleItemsType } from 'src/features/toggle/ToggleType';
+
 import * as S from './style';
 
 const MainPage = () => {
-  return <S.Container></S.Container>;
+  const toggleItems: ToggleItemsType = ['긍정도순', '업종명순'];
+
+  return (
+    <S.Container>
+      <Toggle toggleItems={toggleItems} />
+    </S.Container>
+  );
 };
 
 export default MainPage;
