9e1b56d9a36171928b56019eb7b74d0faead5cd1
cutyapple <dbtldhs0915@naver.com>
2021-04-16 16:54:04 +0900

    [FEAT] <src/library/requestApi.ts>

---
+++ src/library/requestApi.ts
@@ -0,0 +1,98 @@
+import axios from 'axios';
+
+import type { DotEnvType, AccessTokenType } from '../types';
+import type {
+  RequestApiType,
+  RequestApiWithBodyType,
+  RequestApiWithAccessTokenType,
+  RequestApiWithBodyAndAccessTokenType,
+} from '../types/requestApiType';
+
+export const methodType = {
+  POST: 'post',
+  PUT: 'put',
+  GET: 'get',
+  DELETE: 'delete',
+};
+
+const BASE_URL: DotEnvType = process.env.REACT_APP_BASE_URL;
+const LOCAL_STORAGE_ACCESS_TOKEN_NAME = 'accessToken';
+const ACCESS_TOKEN_NAME = 'Bearer Authorization';
+
+export const requestApiWithBodyAndAccessToken = async ({
+  httpMethod,
+  requestUrl,
+  body,
+  headers,
+}: RequestApiWithBodyAndAccessTokenType) => {
+  const accessToken: AccessTokenType = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_NAME);
+
+  try {
+    if (accessToken === null) {
+      throw new Error(`Try ${httpMethod} request with access token, but there is no access token.`);
+    }
+
+    const res = await axios[httpMethod](BASE_URL + requestUrl, body, {
+      ...headers,
+      [ACCESS_TOKEN_NAME]: accessToken,
+    });
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const requestApiWithBody = async ({
+  httpMethod,
+  requestUrl,
+  body,
+  headers,
+}: RequestApiWithBodyType) => {
+  try {
+    const res = await axios[httpMethod](BASE_URL + requestUrl, body, { headers });
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const requestApiWithAccessToken = async ({
+  httpMethod,
+  requestUrl,
+  headers,
+}: RequestApiWithAccessTokenType) => {
+  const accessToken: AccessTokenType = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_NAME);
+
+  try {
+    if (accessToken === null) {
+      throw new Error(`Try ${httpMethod} request with access token, but there is no access token.`);
+    }
+
+    const res = await axios[httpMethod](BASE_URL + requestUrl, {
+      ...headers,
+      [ACCESS_TOKEN_NAME]: accessToken,
+    });
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const requestApi = async ({ httpMethod, requestUrl, headers }: RequestApiType) => {
+  try {
+    const res = await axios[httpMethod](BASE_URL + requestUrl, {
+      headers,
+    });
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const logout = () => {
+  localStorage.clear();
+};
