6cc3d71eff7b93db91037e5f59438145ab2f5a0d
cutyapple <dbtldhs0915@naver.com>
2021-04-16 16:52:55 +0900

    [FEAT] <src/types/requestApiType.ts> set request apis' type

---
+++ src/types/requestApiType.ts
@@ -0,0 +1,22 @@
+export type RequestApiWithBodyAndAccessTokenType = {
+  httpMethod: string;
+  requestUrl: string;
+  body: any;
+  headers: any;
+};
+export type RequestApiWithBodyType = {
+  httpMethod: string;
+  requestUrl: string;
+  body: any;
+  headers: any;
+};
+export type RequestApiWithAccessTokenType = {
+  httpMethod: string;
+  requestUrl: string;
+  headers: any;
+};
+export type RequestApiType = {
+  httpMethod: string;
+  requestUrl: string;
+  headers: any;
+};
