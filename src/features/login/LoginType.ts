6db4e26c282f4b1a042b5678a4e07f7280c4dcec
cutyapple <dbtldhs0915@naver.com>
2021-04-16 18:07:49 +0900

    [FEAT] <src/features/login/LoginType.ts> add Failure Type

--- src/features/login/LoginType.ts
+++ src/features/login/LoginType.ts
@@ -1,4 +1,29 @@
-import { GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
+import { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
 
-export type API_KEY = string | undefined;
 export type GoogleResponseType = GoogleLoginResponse | GoogleLoginResponseOffline;
+
+type LoginDataType =
+  | {}
+  | {
+      accessToken: string;
+      accountName: string;
+      refreshToken: string;
+    };
+export type LoginSliceInitialStateType = {
+  loginLoading: boolean;
+  loginData: LoginDataType;
+  error: null | string;
+};
+
+export type AuthLoginPayloadActionType = {
+  authType: string;
+  'oauth2-token': string;
+};
+export type AuthLoginSuccessPayloadActionType = {
+  accessToken: string;
+  accountName: string;
+  refreshToken: string;
+};
+export type AuthLoginFailurePayloadActionType = {
+  error: string;
+};
