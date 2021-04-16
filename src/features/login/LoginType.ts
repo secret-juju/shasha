06dbc7181aa65f662c799cd9ba84ec8378b3858a
cutyapple <dbtldhs0915@naver.com>
2021-04-16 19:19:13 +0900

    [FEAT] <src/features/login/LoginType.ts> add LoginType

--- src/features/login/LoginType.ts
+++ src/features/login/LoginType.ts
@@ -1,6 +1,26 @@
 import { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
+import { ReactFacebookLoginInfo, ReactFacebookFailureResponse } from 'react-facebook-login';
+import { DotEnvType } from 'src/types';
+
+export type LoginType = {
+  apiKeyProps: {
+    REACT_APP_GOOGLE_API_KEY: DotEnvType;
+    REACT_APP_FACEBOOK_API_KEY: DotEnvType;
+    REACT_APP_NAVER_API_KEY: DotEnvType;
+  };
+  funcProps: {
+    onSuccessGoogleLogin: (response: GoogleResponseType) => void;
+    onFailureGoogleLogin: (error: GoogleFailureResponseType) => void;
+    onSuccessFacebookLogin: (userInfo: FacebookReponseType) => void;
+    onFailureFacebookLogin: (response: FacebookFailureResponseType) => void;
+    initializeNaverLogin: () => void;
+  };
+};
 
 export type GoogleResponseType = GoogleLoginResponse | GoogleLoginResponseOffline;
+export type GoogleFailureResponseType = any;
+export type FacebookReponseType = ReactFacebookLoginInfo | ReactFacebookFailureResponse;
+export type FacebookFailureResponseType = ReactFacebookFailureResponse;
 
 type LoginDataType =
   | {}
@@ -12,7 +32,7 @@ type LoginDataType =
 export type LoginSliceInitialStateType = {
   loginLoading: boolean;
   loginData: LoginDataType;
-  error: null | string;
+  loginError: null | string;
 };
 
 export type AuthLoginPayloadActionType = {
