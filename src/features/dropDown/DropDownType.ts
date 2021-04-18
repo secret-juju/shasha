fb22fcfdb8b116e1ee5ddb1586528c7f6a8b00f5
cutyapple <dbtldhs0915@naver.com>
2021-04-18 12:03:24 +0900

    [FEAT] <src/features/dropDown/DropDownType.ts> add drop down props type

--- src/features/dropDown/DropDownType.ts
+++ src/features/dropDown/DropDownType.ts
@@ -3,9 +3,11 @@ export type DropDownItemType = string;
 export type DropDownItemsType = DropDownItemType[];
 
 export type DropDownPropsType = {
+  active: boolean;
   selectDropDownItem: DropDownItemType;
   unSelectDropDownItems: DropDownItemsType;
   onDropDownItemClick: (dropDownItem: DropDownItemType) => void;
+  onDropDownSectionClick: () => void;
 };
 export type DropDownContainerPropsType = {
   dropDownItems: DropDownItemsType;
