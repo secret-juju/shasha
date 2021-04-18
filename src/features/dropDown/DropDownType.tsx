53ebab5fa7385625e820f8852a092bf8fb413ae3
cutyapple <dbtldhs0915@naver.com>
2021-04-18 11:17:00 +0900

    [FEAT] <src/features/dropDown/DropDownType.tsx> set container type

--- src/features/dropDown/DropDownType.tsx
+++ src/features/dropDown/DropDownType.tsx
@@ -4,5 +4,10 @@ export type DropDownItemsType = DropDownItemType[];
 
 export type DropDownPropsType = {
   selectDropDownItem: DropDownItemType;
+  unSelectDropDownItems: DropDownItemsType;
+  onDropDownItemClick: (dropDownItem: DropDownItemType) => void;
+};
+export type DropDownContainerPropsType = {
   dropDownItems: DropDownItemsType;
+  defaultDropDownItem: DropDownItemType;
 };
