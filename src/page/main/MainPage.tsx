47d4a811b51b04850eccc9a62e690df356d1e87c
cutyapple <dbtldhs0915@naver.com>
2021-04-18 10:53:53 +0900

    [FEAT] <src/page/main/MainPage.tsx> add DropDown and set default prop

--- src/page/main/MainPage.tsx
+++ src/page/main/MainPage.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 
-import Toggle from '../../features/toggle/Toggle';
-
-import { ToggleItemsType } from 'src/features/toggle/ToggleType';
+import type { DropDownItemsType, DropDownItemType } from '../../features/dropDown/DropDownType';
+import type { ToggleItemsType } from '../../features/toggle/ToggleType';
 
 import * as S from './style';
+
+import DropDown from '../../features/dropDown/DropDown';
+import Toggle from '../../features/toggle/Toggle';
 
 const MainPage = () => {
   const toggleItems: ToggleItemsType = ['긍정도순', '업종명순'];
 
+  const selectDropDownItem: DropDownItemType = '업종명순';
+  const dropDownItems: DropDownItemsType = [
+    '업종명순',
+    '종목명순',
+    '긍정도순',
+    '현재가순',
+    '전일비순',
+    '등락률순',
+  ].filter(dropDownItem => dropDownItem !== selectDropDownItem);
+
   return (
     <S.Container>
       <Toggle toggleItems={toggleItems} />
+      <DropDown dropDownItems={dropDownItems} selectDropDownItem={selectDropDownItem} />
     </S.Container>
   );
 };
