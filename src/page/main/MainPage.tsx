75d21d2da7bed55949fa791fad2101d0b4ba128f
cutyapple <dbtldhs0915@naver.com>
2021-04-18 11:17:54 +0900

    [FEAT] <src/page/main/MainPage.tsx> apply DropDownContainer

--- src/page/main/MainPage.tsx
+++ src/page/main/MainPage.tsx
@@ -5,13 +5,13 @@ import type { ToggleItemsType } from '../../features/toggle/ToggleType';
 
 import * as S from './style';
 
-import DropDown from '../../features/dropDown/DropDown';
 import Toggle from '../../features/toggle/Toggle';
+import DropDownContainer from '../../features/dropDown/DropDownContainer';
 
 const MainPage = () => {
   const toggleItems: ToggleItemsType = ['긍정도순', '업종명순'];
 
-  const selectDropDownItem: DropDownItemType = '업종명순';
+  const defaultDropDownItem: DropDownItemType = '업종명순';
   const dropDownItems: DropDownItemsType = [
     '업종명순',
     '종목명순',
@@ -19,12 +19,12 @@ const MainPage = () => {
     '현재가순',
     '전일비순',
     '등락률순',
-  ].filter(dropDownItem => dropDownItem !== selectDropDownItem);
+  ];
 
   return (
     <S.Container>
       <Toggle toggleItems={toggleItems} />
-      <DropDown dropDownItems={dropDownItems} selectDropDownItem={selectDropDownItem} />
+      <DropDownContainer dropDownItems={dropDownItems} defaultDropDownItem={defaultDropDownItem} />
     </S.Container>
   );
 };
