234e5ed7d8fbfaba71b666033c0085f7ab2fa5e0
cutyapple <dbtldhs0915@naver.com>
2021-04-18 22:29:10 +0900

    [TEST] <src/__mocks__/dropDownMocks.ts> add mocks drop down data

---
+++ src/__mocks__/dropDownMocks.ts
@@ -0,0 +1,11 @@
+import { DropDownItemsType, DropDownItemType } from '../features/dropDown/DropDownType';
+
+export const mocksDefaultDropDownItem: DropDownItemType = '업종명순';
+export const mocksDropDownItems: DropDownItemsType = [
+  '업종명순',
+  '종목명순',
+  '긍정도순',
+  '현재가순',
+  '전일비순',
+  '등락률순',
+];
