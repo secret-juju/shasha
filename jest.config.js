7fa839ab8296d85727ab542f0166718fbd85fc4c
cutyapple <dbtldhs0915@naver.com>
2021-04-18 22:28:39 +0900

    [CHORE] <jest.config.js> add jest module svg

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/__test__/config/importJestDOM.ts'],
   testMatch: ['**/(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif)$': '<rootDir>/src/__mocks__/fileMock.ts',
+    '\\.(svg|jpg|jpeg|png|gif)$': '<rootDir>/src/__mocks__/fileMock.ts',
   },
 };
