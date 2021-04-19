153008e04cb049817cf155dd9e2ae87f45a035ef
cutyapple <dbtldhs0915@naver.com>
2021-04-19 23:57:03 +0900

    [CHORE] <jest.config.js> add jest-plugin-context/setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/__test__/config/importJestDOM.ts'],
+  setupFilesAfterEnv: [
+    'jest-plugin-context/setup',
+    '<rootDir>/src/__test__/config/importJestDOM.ts',
+  ],
   testMatch: ['**/(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
     '\\.(svg|jpg|jpeg|png|gif)$': '<rootDir>/src/__mocks__/fileMock.ts',
