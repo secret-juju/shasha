fd15afd7cc65acbba7a34b9099ab4544ce1814a2
cutyapple <dbtldhs0915@naver.com>
2021-04-19 23:17:03 +0900

    [FEAT] <src/features/dropDown/DropDown.tsx> add data test id for testing

--- src/features/dropDown/DropDown.tsx
+++ src/features/dropDown/DropDown.tsx
@@ -20,27 +20,29 @@ const DropDown = ({
   }, [active]);
 
   return (
-    <S.DropDownSection onClick={onDropDownSectionClick} className='drop-down-section'>
-      <S.SelectDropDownItemWrapper active={active}>
-        <S.SelectDropDownItem>{selectDropDownItem}</S.SelectDropDownItem>
-        <S.SelectDropDownArrow src={DropDownArrowSvg} active={active} />
-      </S.SelectDropDownItemWrapper>
-      {unSelectDropDownItems.length ? (
-        <S.DropDownItemWrapper
-          active={active}
-          firstLoading={firstLoading}
-          itemLength={unSelectDropDownItems.length}
-        >
-          {unSelectDropDownItems.map(dropDownItem => (
-            <S.DropDownItem key={dropDownItem} onClick={() => onDropDownItemClick(dropDownItem)}>
-              {dropDownItem}
-            </S.DropDownItem>
-          ))}
-        </S.DropDownItemWrapper>
-      ) : (
-        '드롭다운 데이터가 없습니다'
-      )}
-    </S.DropDownSection>
+    <S.DropDownWrapper data-testid='drop-down-wrapper'>
+      <S.DropDownSection onClick={onDropDownSectionClick} className='drop-down-section'>
+        <S.SelectDropDownItemWrapper active={active}>
+          <S.SelectDropDownItem>{selectDropDownItem}</S.SelectDropDownItem>
+          <S.SelectDropDownArrow src={DropDownArrowSvg} active={active} />
+        </S.SelectDropDownItemWrapper>
+        {unSelectDropDownItems.length ? (
+          <S.DropDownItemWrapper
+            active={active}
+            firstLoading={firstLoading}
+            itemLength={unSelectDropDownItems.length}
+          >
+            {unSelectDropDownItems.map(dropDownItem => (
+              <S.DropDownItem key={dropDownItem} onClick={() => onDropDownItemClick(dropDownItem)}>
+                {dropDownItem}
+              </S.DropDownItem>
+            ))}
+          </S.DropDownItemWrapper>
+        ) : (
+          '드롭다운 데이터가 없습니다'
+        )}
+      </S.DropDownSection>
+    </S.DropDownWrapper>
   );
 };
 
