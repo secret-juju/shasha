87a64920d76a10eb7195eecb6485bc0d1052383a
cutyapple <dbtldhs0915@naver.com>
2021-04-19 18:43:09 +0900

    [FEAT] <src/page/main/MainPage.tsx> apply DropDownContainer complete

--- src/page/main/MainPage.tsx
+++ src/page/main/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import type { DropDownItemsType, DropDownItemType } from '../../features/dropDown/DropDownType';
 import type { ToggleItemsType } from '../../features/toggle/ToggleType';
 
 import * as S from './style';
@@ -11,20 +10,10 @@ import DropDownContainer from '../../features/dropDown/DropDownContainer';
 const MainPage = () => {
   const toggleItems: ToggleItemsType = ['긍정도순', '업종명순'];
 
-  const defaultDropDownItem: DropDownItemType = '업종명순';
-  const dropDownItems: DropDownItemsType = [
-    '업종명순',
-    '종목명순',
-    '긍정도순',
-    '현재가순',
-    '전일비순',
-    '등락률순',
-  ];
-
   return (
     <S.Container>
       <Toggle toggleItems={toggleItems} />
-      <DropDownContainer dropDownItems={dropDownItems} defaultDropDownItem={defaultDropDownItem} />
+      <DropDownContainer />
     </S.Container>
   );
 };
