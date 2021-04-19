71531195271f3a576ce64a93fef62eb2c0bf57d0
cutyapple <dbtldhs0915@naver.com>
2021-04-20 07:39:39 +0900

    [TEST] <src/features/header/test/Header.spec.tsx> coverage 100%

--- src/features/header/test/Header.spec.tsx
+++ src/features/header/test/Header.spec.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { HeaderTestInitialPropsType } from '../HeaderType';
 
 import { LogoPng } from '../../../assets/header';
+
 import Header from '../Header';
 
 describe('components/header', () => {
+  const onAuthButtonClickMock = jest.fn();
+
+  const setUp = (initialProps: HeaderTestInitialPropsType = {}) => {
+    const defaultProps = Object.assign(
+      {
+        isLoginNow: true,
+        onAuthButtonClick: onAuthButtonClickMock,
+      },
+      initialProps,
+    );
+
+    const utils = render(<Header {...defaultProps} />);
+
+    return { utils };
+  };
+
   it(`header has a logo what alt is "secret juju" and src is "LogoPng"`, () => {
-    render(<Header />);
+    setUp();
 
     const logo = screen.getByRole('img');
     expect(logo).toHaveAttribute('alt', 'secret juju');
     expect(logo).toHaveAttribute('src', LogoPng);
+  });
+
+  context('isLoginNow test', () => {
+    it("when isLoginMode is true, show '로그아웃'", () => {
+      setUp();
+
+      screen.getByText('로그아웃');
+    });
+    it("when isLoginMode is false, show '로그인'", () => {
+      setUp({ isLoginNow: false });
+
+      screen.getByText('로그인');
+    });
+  });
+
+  it('click onAuthButtonClick', () => {
+    setUp();
+
+    const headerAuthButton = screen.getByText('로그아웃');
+
+    expect(onAuthButtonClickMock).toBeCalledTimes(0);
+
+    fireEvent.click(headerAuthButton);
+
+    expect(onAuthButtonClickMock).toBeCalledTimes(1);
   });
 });
