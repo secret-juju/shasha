169bc2e1626af98bca5edd0ecdcaa2adbfc91033
cutyapple <dbtldhs0915@naver.com>
2021-04-19 18:43:28 +0900

    [FEAT] <src/module/slice/index.ts> apply dropDownReducer

--- src/module/slice/index.ts
+++ src/module/slice/index.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import loginReducer from '../../features/login/LoginSlice';
+import dropDownReducer from '../../features/dropDown/DropDownSlice';
 
-const rootReducer = combineReducers({ login: loginReducer });
+const rootReducer = combineReducers({ login: loginReducer, dropDown: dropDownReducer });
 
 export type RootReducerType = ReturnType<typeof rootReducer>;
 
