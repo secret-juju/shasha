85a2ab645c667a996b8b16ca0cd1b44e208c92d2
CutyApple <dbtldhs0915@naver.com>
2021-04-28 07:58:59 +0900

    [MODIFY] <src/component/~/LoginRedirect.tsx> apply storage library

--- src/component/loginRedirect/LoginRedirect.tsx
+++ src/component/loginRedirect/LoginRedirect.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Redirect, useLocation } from 'react-router';
 
+import { setLocalStorageItem } from '../../library/storage/storage';
+
 const LoginRedirect = () => {
   const location = useLocation();
 
   if (location?.hash.includes('access_token')) {
-    localStorage.setItem('accessToken', location.hash.split('=')[1]);
+    setLocalStorageItem('accessToken', location.hash.split('=')[1]);
   }
 
   return <Redirect to='/' />;
