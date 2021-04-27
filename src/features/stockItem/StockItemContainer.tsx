c85eb59c9275db9641b612fe73f91ebf8f14777e
CutyApple <dbtldhs0915@naver.com>
2021-04-27 23:49:48 +0900

    [FEAT] <src/features/~/StockItemContainer.tsx> apply props type

--- src/features/stockItem/StockItemContainer.tsx
+++ src/features/stockItem/StockItemContainer.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
+import type { StockThemeType, StockItemContainerPropsType } from './StockItemType';
+
 import { numberAddedComma, removePercentSign } from '../../utils/StringUtils';
 import { half2StockThemeText, sign2StockThemeText } from '../../utils/StockUtils';
 
 import StockItem from './StockItem';
 
-const StockItemContainer = ({ name, positivity, curPrice, diff, fluctuation }) => {
+const StockItemContainer = ({
+  name,
+  positivity,
+  curPrice,
+  diff,
+  fluctuation,
+}: StockItemContainerPropsType) => {
   const curPriceWithComma = numberAddedComma(curPrice);
 
-  const positivityStockTheme = half2StockThemeText(removePercentSign(positivity));
-  const diffStockTheme = sign2StockThemeText(diff);
-  const fluctuationStockTheme = sign2StockThemeText(removePercentSign(fluctuation));
+  const positivityStockTheme: StockThemeType = half2StockThemeText(removePercentSign(positivity));
+  const diffStockTheme: StockThemeType = sign2StockThemeText(diff);
+  const fluctuationStockTheme: StockThemeType = sign2StockThemeText(removePercentSign(fluctuation));
 
   const stockItemValues = { name, positivity, curPriceWithComma, diff, fluctuation };
   const stockItemThemes = {
