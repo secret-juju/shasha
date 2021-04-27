f94c4edeb4164fbb732a20ca06c31024aebb092c
CutyApple <dbtldhs0915@naver.com>
2021-04-27 10:17:34 +0900

    .

--- src/features/login/LoginContainer.tsx
+++ src/features/login/LoginContainer.tsx
@@ -8,6 +8,7 @@ import { GoogleFailureResponseType, GoogleResponseType } from './LoginType';
 import { useAppDispatch, useTypedSelector } from '../../module/store';
 
 import { authLogin, loginData, loginSliceState } from './LoginSlice';
+import axios from 'axios';
 
 const { naver } = window as any;
 
@@ -21,15 +22,39 @@ const LoginContainer = () => {
 
   const onSuccessGoogleAuth = async (response: GoogleResponseType) => {
     if ('tokenId' in response) {
-      dispatch(authLogin({ authType: 'google', 'oauth2-token': response.tokenId }));
+      const res = await dispatch(
+        authLogin({ authType: 'google', 'oauth2-token': response.tokenId }),
+      );
+      console.log(`res`);
+      console.log(res);
     }
   };
   const onFailureGoogleAuth = (error: GoogleFailureResponseType) => {
     alert('구글 로그인에 실패하였습니다. 다시 시도해주세요.');
   };
 
-  const onSuccessFacebookAuth = userInfo => {
+  const onSuccessFacebookAuth = async userInfo => {
     console.log(userInfo);
+
+    try {
+      const res = await axios.get(
+        `https://graph.facebook.com/v2.8/me?fields=id&access_token=${userInfo.accessToken}`,
+      );
+
+      dispatch(authLogin({ authType: 'facebook', 'oauth2-token': res.data.id }));
+      // const res = await axios.post('http://localhost:3000/v1/facebook_auth', {
+      //   token: `${userInfo.accessToken}`,
+      // });
+
+      // const res = await axios.get(
+      //   `https://graph.facebook.com/v10.0/oauth/access_token?client_id=${305208847694209}&redirect_uri=${'http://localhost:3000/'}&client_secret=${'a467182b1fe8e32367205fa6ac66a237'}&code=${
+      //     userInfo.accessToken
+      //   }`,
+      // );
+      console.log(res);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
   const onFailureFacebookAuth = response => {
     alert('페이스북 로그인에 실패하였습니다. 다시 시도해주세요.');
