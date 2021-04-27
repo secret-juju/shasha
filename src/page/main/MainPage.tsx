49f206eb87df5e8a29d48df8da6d41f597a5f5ce
CutyApple <dbtldhs0915@naver.com>
2021-04-27 19:58:03 +0900

    [FEAT] <src/page/main/MainPage.tsx> apply HeaderContainer

--- src/page/main/MainPage.tsx
+++ src/page/main/MainPage.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import * as S from './style';
 
+import HeaderContainer from '../../features/header/HeaderContainer';
 import DropDownContainer from '../../features/dropDown/DropDownContainer';
+import StockWrapper from '../../component/stockWrapper/StockWrapper';
 
 const MainPage = () => {
   return (
     <S.Container>
+      <HeaderContainer />
       <DropDownContainer />
+      <StockWrapper />
     </S.Container>
   );
 };
