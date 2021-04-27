52084682449b0bb0c3ae33361f4fa9921a3b7954
CutyApple <dbtldhs0915@naver.com>
2021-04-27 23:34:54 +0900

    [FEAT] <src/themes/StockTheme.ts> export stock themes

---
+++ src/themes/StockTheme.ts
@@ -0,0 +1,29 @@
+import {
+  BlackBarSvg,
+  BlackNormalFaceSvg,
+  BlueBottomTriangleSvg,
+  BlueSadFaceSvg,
+  RedHappyFaceSvg,
+  RedTopTriangleSvg,
+} from '../assets/stockItem';
+
+export const StockTheme = {
+  default: '#2F2F2F',
+  positive: '#F51818',
+  negative: '#1B61D1',
+  zero: '#2F2F2F',
+};
+
+export const StockFaceTheme = {
+  default: BlackNormalFaceSvg,
+  positive: RedHappyFaceSvg,
+  negative: BlueSadFaceSvg,
+  zero: BlackNormalFaceSvg,
+};
+
+export const StockSignTheme = {
+  default: BlackBarSvg,
+  positive: RedTopTriangleSvg,
+  negative: BlueBottomTriangleSvg,
+  zero: BlackBarSvg,
+};
