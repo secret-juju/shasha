9addfba82585d22c0c37f0e6cfe8ce568669438b
CutyApple <dbtldhs0915@naver.com>
2021-04-27 10:17:25 +0900

    [REFACTOR] <src/page/main> remove un-used code

--- src/page/main/MainPage.tsx
+++ src/page/main/MainPage.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
-import type { ToggleItemsType } from '../../features/toggle/ToggleType';
-
 import * as S from './style';
 
-import Toggle from '../../features/toggle/Toggle';
 import DropDownContainer from '../../features/dropDown/DropDownContainer';
 
 const MainPage = () => {
-  const toggleItems: ToggleItemsType = ['긍정도순', '업종명순'];
-
   return (
     <S.Container>
-      <Toggle toggleItems={toggleItems} />
       <DropDownContainer />
     </S.Container>
   );
