5a609531d5f6439773a23baec0d3429de42a99dc
CutyApple <dbtldhs0915@naver.com>
2021-04-28 07:58:26 +0900

    [FEAT] <src/library/storage/storage.ts> apply branch return value

--- src/library/storage/storage.ts
+++ src/library/storage/storage.ts
@@ -3,12 +3,21 @@ import type { ItemKeyType } from './StorageType';
 export const getLocalStorageItem = (itemKey: ItemKeyType) => {
   const targetItem = localStorage.getItem(itemKey);
 
+  if (typeof targetItem === 'string' && targetItem.split[0] !== `"`) {
+    return targetItem;
+  }
+
   const parseTargetItem = JSON.parse(targetItem);
 
   return parseTargetItem;
 };
 
 export const setLocalStorageItem = (itemKey: ItemKeyType, itemValue) => {
+  if (typeof itemValue === 'string') {
+    localStorage.setItem(itemKey, itemValue);
+    return;
+  }
+
   const stringfyValue = JSON.stringify(itemValue);
 
   localStorage.setItem(itemKey, stringfyValue);
