28c883751cab31bb715c9058a28158acfb5bd34f
CutyApple <dbtldhs0915@naver.com>
2021-04-27 21:34:22 +0900

    [FEAT] <src/utils/StringUtils.ts> add isPercentPositive

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,10 +1,20 @@
-import type { numberAddedCommaProp } from './type/StringUtilsType';
+import type { NumberAddedCommaPropType, PercentType } from './type/StringUtilsType';
 
-export const numberAddedComma = (number: numberAddedCommaProp) => {
+export const numberAddedComma = (number: NumberAddedCommaPropType) => {
   return String(number)
     .split('')
     .reverse()
     .map((n, index) => (index === 0 || index % 3 ? n : n + ','))
     .reverse()
     .join('');
+};
+
+export const isPercentPostive = (percentNumber: PercentType) => {
+  const splitedPercentNumber = percentNumber.split('');
+
+  splitedPercentNumber.pop();
+
+  const numberWithoutPercent = splitedPercentNumber.join('');
+
+  return Number(numberWithoutPercent) >= 0;
 };
