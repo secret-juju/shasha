93d3ec363dccf9028b4f60dcde04bd7003dffef7
CutyApple <dbtldhs0915@naver.com>
2021-05-03 15:02:00 +0900

    [FEAT] <src/component/~/LoginRedirect.tsx> apply dispatch login

--- src/component/loginRedirect/LoginRedirect.tsx
+++ src/component/loginRedirect/LoginRedirect.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
-import { Redirect, useLocation } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router';
 
-import { setLocalStorageItem } from '../../library/storage/storage';
+import { authLogin } from '../../features/login/LoginSlice';
 
 const LoginRedirect = () => {
+  const dispatch = useDispatch();
   const location = useLocation();
 
   if (location?.hash.includes('access_token')) {
-    setLocalStorageItem('accessToken', location.hash.split('=')[1]);
+    const naverAccessToken = location.hash.split('=')[1].split('&')[0];
+
+    dispatch(authLogin({ authType: 'naver', Authorization: naverAccessToken }));
   }
 
-  return <Redirect to='/' />;
+  return <div></div>;
 };
 
 export default LoginRedirect;
