87eeb84c5b1947de8523e346d266e402621a46b9
CutyApple <dbtldhs0915@naver.com>
2021-05-17 22:31:51 +0900

    [FEAT] <src/module/slice/index.ts> apply stockReducer

--- src/module/slice/index.ts
+++ src/module/slice/index.ts
@@ -2,8 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import loginReducer from '../../features/login/LoginSlice';
 import dropDownReducer from '../../features/dropDown/DropDownSlice';
+import stockReducer from '../../features/stock/StockSlice';
 
-const rootReducer = combineReducers({ login: loginReducer, dropDown: dropDownReducer });
+const rootReducer = combineReducers({
+  login: loginReducer,
+  dropDown: dropDownReducer,
+  stock: stockReducer,
+});
 
 export type RootReducerType = ReturnType<typeof rootReducer>;
 
