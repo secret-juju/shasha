a34d579542d384ec9155c70108c83497e4ecd445
CutyApple <dbtldhs0915@naver.com>
2021-05-24 14:51:34 +0900

    [FEAT] <src/utils/authUtils/index.ts>

---
+++ src/utils/authUtils/index.ts
@@ -0,0 +1,31 @@
+import { clearLocalStorage, getLocalStorageItem } from '../../library/storage/storage';
+import { LOCAL_STORAGE_ACCESS_TOKEN_NAME } from '../../library/requestLib';
+
+export const checkAccessTokenEffective = async () => {
+  const accessToken = getLocalStorageItem(LOCAL_STORAGE_ACCESS_TOKEN_NAME);
+
+  if (accessToken) {
+    return false;
+  }
+};
+
+export const checkIsLogin = async () => {
+  try {
+    const accessToken = getLocalStorageItem(LOCAL_STORAGE_ACCESS_TOKEN_NAME);
+
+    if (!accessToken) {
+      throw Error('There is no access token.');
+    }
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const logoutAndRefresh = () => {
+  clearLocalStorage();
+  alert('로그아웃 되었습니다.');
+  console.log(location);
+  location.href = '/login';
+};
