7670b9c56e6ff74f2f18c88d8c0a3ee8776c36dd
CutyApple <dbtldhs0915@naver.com>
2021-05-24 11:03:28 +0900

    [FEAT] <src/features/stock/StockItem.tsx> apply container

---
+++ src/features/stock/StockItem.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { StockFaceTheme, StockSignTheme } from '../../themes/StockTheme';
+
+import { numberAddedComma } from '../../utils/stringUtils';
+
+import * as S from './style';
+
+import { StockItemPropsType } from './StockType';
+
+const StockItem = ({ stockItemValues, stockItemThemes }: StockItemPropsType) => {
+  const {
+    averagePositivity,
+    currentPrice,
+    differenceFromYesterday,
+    fluctuationRate,
+    name,
+  } = stockItemValues;
+  const {
+    averagePositivityStockTheme,
+    differenceFromYesterdayStockTheme,
+    fluctuationRateStockTheme,
+  } = stockItemThemes;
+
+  return (
+    <S.StockItem>
+      <S.StockName>{name}</S.StockName>
+      <S.StockPositivity>
+        <S.PositivityImage src={StockFaceTheme[averagePositivityStockTheme]} />
+        <S.PositivityValue positivityStockTheme={averagePositivityStockTheme}>
+          {averagePositivity}%
+        </S.PositivityValue>
+      </S.StockPositivity>
+      <S.StockCurPrice>{numberAddedComma(currentPrice)}</S.StockCurPrice>
+      <S.StockDiff>
+        <S.DiffValue differenceFromYesterdayStockTheme={differenceFromYesterdayStockTheme}>
+          {differenceFromYesterday}
+        </S.DiffValue>
+        <S.DiffSign src={StockSignTheme[differenceFromYesterdayStockTheme]} />
+      </S.StockDiff>
+      <S.StockFluctuation fluctuationRateStockTheme={fluctuationRateStockTheme}>
+        {fluctuationRate}%
+      </S.StockFluctuation>
+    </S.StockItem>
+  );
+};
+
+export default StockItem;
