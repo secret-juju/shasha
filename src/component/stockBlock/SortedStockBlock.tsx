effb1696c0cf98dbf7d11b1b6aa9bbcf0dfa35c5
CutyApple <dbtldhs0915@naver.com>
2021-05-24 11:08:53 +0900

    [FEAT] <src/~/stockBlock/SortedStockBlock.tsx>

---
+++ src/component/stockBlock/SortedStockBlock.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import * as S from './style';
+
+import StockItemContainer from '../../features/stock/StockItemContainer';
+
+const SortedStockBlock = ({ companies }) => {
+  return (
+    <S.StockBlockArticle>
+      <S.StockListHeader>
+        <S.StockListHeaderText>종목명</S.StockListHeaderText>
+        <S.StockListHeaderText>긍정도</S.StockListHeaderText>
+        <S.StockListHeaderText textAlign='right'>현재가</S.StockListHeaderText>
+        <S.StockListHeaderText textAlign='right'>전일비</S.StockListHeaderText>
+        <S.StockListHeaderText textAlign='right'>등락률</S.StockListHeaderText>
+      </S.StockListHeader>
+      <S.StockItemWrapper>
+        {companies?.map(company => (
+          <StockItemContainer key={company?.name} {...company} />
+        ))}
+      </S.StockItemWrapper>
+    </S.StockBlockArticle>
+  );
+};
+
+export default SortedStockBlock;
