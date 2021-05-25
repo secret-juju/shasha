1fa03f73637cd65ca5c5d7e3d6b55af8d1a3d1a5
CutyApple <dbtldhs0915@naver.com>
2021-05-25 22:36:12 +0900

    [FEAT] <src/routers/MainRouter.tsx> Apply StockPage

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 
 import MainPage from '../page/main/MainPage';
+import StockPage from '../page/stock/StockPage';
 import LoginPage from '../page/login/LoginPage';
 import LoginRedirect from '../component/loginRedirect/LoginRedirect';
 
@@ -9,6 +10,7 @@ const MainRouter = () => {
   return (
     <Switch>
       <Route exact path='/' component={MainPage} />
+      <Route path='/stock/:companyId' component={StockPage} />
       <Route exact path='/login' component={LoginPage} />
       <Route exact path='/login/naver/redirect' component={LoginRedirect} />
     </Switch>
