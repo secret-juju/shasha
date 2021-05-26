bddabc646611a5212f1b42f64a650be6b179a285
CutyApple <dbtldhs0915@naver.com>
2021-05-26 11:08:57 +0900

    [FEAT] <src/page/stock/StockPage.tsx> apply StockPageItem

--- src/page/stock/StockPage.tsx
+++ src/page/stock/StockPage.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import * as S from './style';
 
 import HeaderContainer from '../../features/header/HeaderContainer';
+import StockPageItem from '../../features/stockPage/StockPageItem';
 
 const StockPage = () => {
   return (
     <S.StockPageContainer>
       <HeaderContainer />
-      <S.StockWrapper></S.StockWrapper>
+      <S.StockWrapper>
+        <StockPageItem />
+      </S.StockWrapper>
     </S.StockPageContainer>
   );
 };
